c93241a5b995f2e1406fad0481daa99d2681f7d0
yxluo <1625436933@qq.com>
2024-07-16 09:17:03 +0800

    update init

--- model/__init__.py
+++ model/__init__.py
@@ -1,9 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-# from flask_migrate import Migrate
 
 db = SQLAlchemy()
-# migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
@@ -12,7 +10,6 @@ def create_app():
     app.config.from_object(DBconfig)
 
     db.init_app(app)
-    # migrate.init_app(app,db)
     with app.app_context():
         from api import register_routes
 
